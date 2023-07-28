4a3df2d877d26e7dc2bd3441bcd1d0e11a550442
Hossein movahed <hossein.movahed@monese.com>
2023-07-28 23:36:22 +0200

    fix Ci failed

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,13 +2,4 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
-  rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        'ignoreAtRules': ['tailwind']
-      }
-    ],
-  }
 };
