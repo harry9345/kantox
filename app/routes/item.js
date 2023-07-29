07d626ff39081039d444d0c603f8c3b2226660bf
Hossein movahed <hossein.movahed@monese.com>
2023-07-29 20:31:31 +0200

    update cart ui

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,8 +1,16 @@
 import Route from "@ember/routing/route";
 
+import { service } from "@ember/service";
+
 export default class ItemRoute extends Route {
-  model(params) {
+  @service store;
+
+  async model(params) {
     const { item_id } = params;
-    return item_id;
+    const allProducts = await this.store.findAll("product");
+    const product = allProducts.findBy('id', item_id);
+
+    console.log('product', product);
+    return product;
   }
 }
