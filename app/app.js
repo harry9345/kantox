bf71215085f99350eec1dd37ed476ddba13a185d
Hossein movahed <hossein.movahed@monese.com>
2023-08-04 03:05:56 +0200

    rename css file

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import Application from "@ember/application";
 import Resolver from "ember-resolver";
 import loadInitializers from "ember-load-initializers";
 import config from "kshop-app/config/environment";
-import "kshop-app/app.css";
+import "kshop-app/kshop-app.css";
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
